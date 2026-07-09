7c31dc28254ef2b715209b6a19fa9989a9ae16a2
Crisiszzz07 <ristinaorozco2006@gmail.com>
2026-07-08 23:58:08 -0500

    validaciones para evitar flags obligatorias vacías

--- main.go
+++ main.go
@@ -16,17 +16,30 @@ func check(e error) { //función para errores en caso de que hayan
 
 func main() {
 
+	//CONFIGURACIÓN DE FLAGS: El programa configura mas no lee aún el contenido de la terminal
+	//los flag.type (ej: flag.String) reserva el espacio de la memoria para el cotenido y entrega el puntero
+
 	//flag para targetHash
 
-	hashPtr := flag.String("hash", "", "hash password that wants to be cracked")
+	hashPtr := flag.String("hash", "", "OBLIGATORY: hash password that wants to be cracked")
 
 	//flag para diccionario
-	dicPtr := flag.String("d", "", "dictionary to use")
+	dicPtr := flag.String("d", "", "OBLIGATORY: dictionary to use")
 
 	//TODO
 	//flag para directorio para almacenar un archivo resultante con los resultados que coincidan
 	//flag para distintas opciones de algoritmos
-	flag.Parse()
+	flag.Parse() //Go lee el contenido en la terminal al hacer parsing y almacena teniendo en
+	//cuenta el espacio de memoria al que hace referencia el puntero
+
+	//para evitar el panic por intentar leer un archivo vacío, se hace una validación de
+	//argumentos que son obligatorios
+	if *hashPtr == "" || *dicPtr == "" {
+		fmt.Println("Error: there are some missing arguments. Use -h or -help for details.")
+		return
+	}
+
+	//se hace una dereferencia para obtener el contenido al que el puntero hace referencia
 	targetHash := *hashPtr
 	filePath := *dicPtr
 
@@ -48,5 +61,7 @@ func main() {
 			fmt.Println("La contraseña es:", scanner.Text())
 			break
 		}
+
 	}
+	fmt.Println("No se encontró ninguna contraseña que coincidiera.")
 }
